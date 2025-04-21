8c5f74c51f13e57aa5a3127656ebfff8139aa3af
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-04-21 20:58:30 -0300

    solve 06_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const divisibleFour = year % 4 === 0;
+    const divisible100 = year % 100 === 0;
+    const divisible400 = year % 400 === 0;
+    
+    if(divisibleFour && (!divisible100 || divisible400)) {
+        return true;
+    }else { return false }
+    
 };
 
 // Do not edit below this line
