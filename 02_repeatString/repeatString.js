9b92c7a4eb1c6dfc753d784afbdb993e592c757f
Paulo Almeida <145488386+Phsla00@users.noreply.github.com>
2025-04-21 13:43:29 -0300

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(strg, num) {
+    let string = "";
+    if(num < 0) return "ERROR";
+    for(let i = 0; i < num; i++) {
+        string += strg;
+    }
+    return string;
 };
 
 // Do not edit below this line
